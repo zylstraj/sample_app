44b0199dd3b4b99dc4523c8150de631b092b7d6f
Josh Zylstra <zylstrajosh@gmail.com>
2017-04-23 13:53:28 -0700

    Building out blog
    Learning how this all works, more and more - having an understanding.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'posts#index'
+root 'posts#index', as: 'home'
 
-get 'about' => 'pages#about'
+get 'about' => 'pages#about', as: 'about'
 #example route
 #get '/products/:id' => 'catalog#view'
 # catalog is the controller and view - view
@@ -13,4 +13,9 @@ Working on my multi-line comments and it's a get request to
 the pages controller
 and to the about view
 =end
+
+resources :posts
+# Example resource route - maps http verbs to controller actions
+
+
 end
