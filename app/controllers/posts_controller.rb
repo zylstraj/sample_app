04eafa3df9aae8ac6c76b3446d0746c5dd6d3196
Josh Zylstra <zylstrajosh@gmail.com>
2017-05-13 15:26:18 -0700

    The Edit is working
    Not a ton - - of --- it's all hidden right now - - I can get to it
    through the API/links and so could others, but no way to click to get
    there and update.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,18 @@ class PostsController < ApplicationController
       #render plain: params[:post].inspect
       #simple - way to show the object
   end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    if(@post.update(post_params))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
     private def post_params
       params.require(:post).permit(:title, :body)
     end
